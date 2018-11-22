d2b2dc965d74e665b1e42bd26f5c742608282228
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-11-21 23:28:00 -0800

    Initial commit

---
+++ src/Controller/PeopleController.php
@@ -0,0 +1,51 @@
+<?php
+    namespace App\Controller;
+
+    use App\Entity\Person;
+
+    use Symfony\Component\HttpFoundation\Response; 
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+    class PeopleController extends Controller{
+
+        /**
+         * @Route("/")
+         * @Method({"GET"})
+         */
+        public function index() {
+            $people= $this->getDoctrine()->getRepository(Person::class)->findAll();
+            return $this->render('people/index.html.twig', array('people' => $people));
+        }
+
+        /**
+        * @Route("/people/{id}", name="person_show")
+        */
+        public function show($id) {
+            $person = $this->getDoctrine()->getRepository(Person::class)->find($id);
+            return $this->render('people/show.html.twig', array('person' => $person));
+        }
+        
+        
+        /**
+         * @Route("/people/updateContacted/{id}")
+         */
+        public function updateContacted($id)
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $person = $entityManager->getRepository(Person::class)->find($id);
+        
+            if (!$person) {
+                throw $this->createNotFoundException(
+                    'No product found for id '.$id
+                );
+            }
+
+            $person->updateLastContacted();
+            $entityManager->flush();
+            $people= $this->getDoctrine()->getRepository(Person::class)->findAll();
+            return $this->render('people/index.html.twig', array('people' => $people));
+        
+    }
+}
---
+++ src/Entity/Person.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ */
+class Person
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $relationship;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $details;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastContacted;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $username;
+
+    //Gettters and Setters 
+
+    public function updateLastContacted(){
+        $this->lastContacted = new \DateTime("now");
+    }
+
+    public function getLastContacted(){
+        return $this->lastContacted->format('Y-m-d H');
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getUsername(){
+        return $this->username;
+    }
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function getDetails(){
+        return $this->details;
+    }
+
+    public function setDetails($details){
+        $this->name = $details;
+    }
+
+    public function getRelationship(){
+        return $this->relationship;
+    }
+
+    public function setRelationship($relationship){
+        $this->relationship = $relationship;
+    }
+    
+}
